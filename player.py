c864d2e36e3fea4af8535b5ff05d2f1dff60a962
Somnath Banerjee <somnathbanerjee342000@gmail.com>
2026-07-07 13:04:05 +0530

    Update player.py
    প

--- player.py
+++ player.py
@@ -1,22 +1,61 @@
 import streamlit as st
 import pandas as pd
+from sheet import connect_sheet
 
-df = pd.read_excel("players.xlsx")
+# Players.xlsx GitHub root folder-এ থাকবে
+df = pd.read_excel("Players.xlsx")
 
 
-def select_player(group, key):
+def load_group(group):
 
     data = df[df["Group"] == group]
 
-    p = st.selectbox(
-        "Select Player",
+    return data
+
+
+def select_player(match_id, player, group, ptype):
+
+    ws = connect_sheet().worksheet("Selection")
+
+    rows = ws.get_all_values()
+
+    # Player already submitted?
+    for r in rows[1:]:
+        if (
+            r[0] == match_id and
+            r[1] == player and
+            r[2] == ptype
+        ):
+            st.success(f"{ptype} already selected : {r[3]}")
+            return
+
+    data = load_group(group)
+
+    name = st.selectbox(
+        f"Select {ptype}",
         data["Player Name"],
-        key=key
+        key=f"{player}_{ptype}_{group}"
     )
 
-    row = data[data["Player Name"] == p].iloc[0]
+    link = data.loc[
+        data["Player Name"] == name,
+        "Hyperlink"
+    ].values[0]
 
-    st.write("ID :", row["ID"])
-    st.write("Group :", row["Group"])
+    st.caption(link)
 
-    return p
+    if st.button(
+        f"Confirm {ptype}",
+        key=f"btn_{player}_{ptype}"
+    ):
+
+        ws.append_row([
+            match_id,
+            player,
+            ptype,
+            name,
+            group,
+            link
+        ])
+
+        st.success(f"{name} Selected")
