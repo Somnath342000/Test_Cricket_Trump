39fc8466e7b2e4ed3828b3d16ba01f53d003ffcb
Somnath Banerjee <somnathbanerjee342000@gmail.com>
2026-07-08 16:54:42 +0530

    Update score.py
    A

--- score.py
+++ score.py
@@ -1,38 +1,47 @@
-from sheet import get_sheet
-
-def add_score(match_id, player, point):
-
-    ws = get_sheet("Scores")
-
-    rows = ws.get_all_values()
-
-    for i, r in enumerate(rows[1:], start=2):
-
-        if r[0]==match_id and r[1]==player:
-
-            total=int(r[2])+point
-
-            ws.update(f"C{i}", total)
-
-            return
-
-    ws.append_row([match_id, player, point])
+from sheet import get_scores
 
 
-def result(match_id):
+def total_score(match_id, post):
 
-    ws=get_sheet("Scores")
+    rows = get_scores(
+        match_id,
+        post
+    )
 
-    rows=ws.get_all_values()
+    score = {
+        "Som": 0,
+        "Joy": 0,
+        "Krish": 0
+    }
 
-    score=[]
+    for r in rows:
 
-    for r in rows[1:]:
+        score["Som"] += float(
+            r["Som"]
+        )
 
-        if r[0]==match_id:
+        score["Joy"] += float(
+            r["Joy"]
+        )
 
-            score.append((r[1],int(r[2])))
-
-    score.sort(key=lambda x:x[1],reverse=True)
+        score["Krish"] += float(
+            r["Krish"]
+        )
 
     return score
+
+
+def result(match_id, post):
+
+    score = total_score(
+        match_id,
+        post
+    )
+
+    data = sorted(
+        score.items(),
+        key=lambda x: x[1],
+        reverse=True
+    )
+
+    return data
