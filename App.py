ee0aa39466834b794a390262f75125404cacaea6
Somnath Banerjee <somnathbanerjee342000@gmail.com>
2026-07-09 12:51:30 +0530

    Update App.py
    Ok

--- App.py
+++ App.py
@@ -3,32 +3,16 @@ import pandas as pd
 import random
 import string
 
-from sheet import (
-    connect_sheet,
-    create_match,
-    match_exists,
-    get_match,
-    all_groups_locked
-)
+# ব্র্যাকেট সরিয়ে এক লাইনে আনা হয়েছে যেন কোনো ইনডেন্টেশন এরর না হয়
+from sheet import connect_sheet, create_match, match_exists, get_match, all_groups_locked
 
-from draft import (
-    batting_toss,
-    bowling_toss,
-    show_toss
-)
+from draft import batting_toss, bowling_toss, show_toss
 
-from group import (
-    draft_screen
-)
+from group import draft_screen
 
-from player import (
-    selection_page
-)
+from player import selection_page
 
-from score import (
-    total_score,
-    result
-)
+from score import total_score, result
 
 # ==========================================
 # PAGE CONFIG
